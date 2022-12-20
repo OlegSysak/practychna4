8b54d2ae3448b2d8f864af6ddf1c045a722ca9bf
OlegSysak <oleh.r.sysak@ukd.edu.ua>
2022-12-20 21:20:21 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,512 @@
+#s = (f" \n #### \n# \n# \n ### \n    # \n    # \n ### \n")
+#y = (f"\n#    # \n#    # \n #  # \n  # \n  # \n  # \n  # \n")
+#a = (f" \n  ### \n #   # \n #   # \n ##### \n #   # \n #   # \n #   # \n")
+#k = (f" \n #   # \n #  # \n # # \n ## \n # # \n #  # \n #   # \n" )
+#o = (f" \n *** \n*   * \n*   * \n*   * \n*   * \n*   * \n *** \n")
+#l = (f" \n * \n * \n * \n * \n * \n * \n ***** \n")
+#e = (f" \n ***** \n * \n * \n *** \n * \n * \n ***** \n")
+#d = (f" \n ####  \n #   # \n #   # \n #   # \n #   # \n #   # \n #### ")
+#r = (f" \n #### \n #   # \n #   # \n #### \n # # \n #  # \n #   # ")
+#n = (f" \n #    # \n #    # \n ##   # \n # #  # \n #   ## \n #    # \n #    #")
+#b = (f" \n ####  \n #   # \n #   # \n ####  \n #   # \n #   # \n ####   ")
+#c = (f" \n  ###  \n #   # \n # \n # \n # \n # \n # \n #   # \n  ### ")
+#g = (f" \n  ### \n #   # \n # \n #  ## \n #   # \n #   # \n  ### ")
+#h = (f" \n #   # \n #   # \n #   # \n ##### \n #   # \n #   # \n #   #")
+#i = (f" \n ### \n  # \n  # \n  # \n  # \n  # \n ###")
+#j = (f" \n ### \n  # \n  # \n  # \n  # \n  # \n  # \n##  ")
+#m = (f" \n #   # \n #   # \n ## ## \n # # # \n #   # \n #   # \n #   # ")
+#p = (f" \n #### \n #   # \n #   # \n #### \n # \n # \n #")
+#q = (f" \n  ### \n #   # \n #   # \n #   # \n #   # \n #  # \n  ## #")
+#t = (f" \n ##### \n   # \n   # \n   # \n   # \n   # \n   # ")
+#u = (f" \n #   # \n #   # \n #   # \n #   # \n #   # \n #   # \n  ### ")
+#w = (f" \n #   # \n #   # \n #   # \n # # # \n ## ## \n #   # \n #   # ")
+#x = (f"\n #   # \n #   # \n  # # \n   #  \n  # # \n #   # \n #   #")
+#z = (f" \n ##### \n     # \n    # \n   # \n  # \n # \n #####")
+#f = (f"\n##### \n# \n# \n### \n# \n# \n#")
+#v = (f"\n#   # \n#   # \n#   # \n#   # \n#   # \n # # \n  # ")
+def a():
+    print(f" \n  ### \n #   # \n #   # \n ##### \n #   # \n #   # \n #   # \n")
+def b():
+    print(f" \n ####  \n #   # \n #   # \n ####  \n #   # \n #   # \n ####   ")
+def c():
+    print(f" \n  ###  \n #   # \n # \n # \n # \n # \n # \n #   # \n  ### ")
+def s():
+    print(f" \n #### \n# \n# \n ### \n    # \n    # \n ### \n")
+def y():
+    print(f"\n#    # \n#    # \n #  # \n  # \n  # \n  # \n  # \n")
+def k():
+    print(f" \n #   # \n #  # \n # # \n ## \n # # \n #  # \n #   # \n" )
+def o():
+    print(f" \n *** \n*   * \n*   * \n*   * \n*   * \n*   * \n *** \n")
+def l():
+    print(f" \n * \n * \n * \n * \n * \n * \n ***** \n")
+def e():
+    print(f" \n ***** \n * \n * \n *** \n * \n * \n ***** \n")
+def d():
+    print(f" \n ####  \n #   # \n #   # \n #   # \n #   # \n #   # \n #### ")
+def r():
+    print(f" \n #### \n #   # \n #   # \n #### \n # # \n #  # \n #   # ")
+def g():
+    print(f" \n  ### \n #   # \n # \n #  ## \n #   # \n #   # \n  ### ")
+def n():
+    print(f" \n #    # \n #    # \n ##   # \n # #  # \n #   ## \n #    # \n #    #")
+def h():
+    print(f" \n #   # \n #   # \n #   # \n ##### \n #   # \n #   # \n #   #")
+def i():
+    print(f" \n ### \n  # \n  # \n  # \n  # \n  # \n ###")
+def j():
+    print(f" \n ### \n  # \n  # \n  # \n  # \n  # \n  # \n##  ")
+def m():
+    print(f" \n #   # \n #   # \n ## ## \n # # # \n #   # \n #   # \n #   # ")
+def p():
+    print(f" \n #### \n #   # \n #   # \n #### \n # \n # \n #")
+def q():
+    print(f" \n #### \n #   # \n #   # \n #### \n # \n # \n #")
+def t():
+    print(f" \n ##### \n   # \n   # \n   # \n   # \n   # \n   # ")
+def u():
+    print(f" \n #   # \n #   # \n #   # \n #   # \n #   # \n #   # \n  ### ")
+def w():
+    print(f" \n #   # \n #   # \n #   # \n # # # \n ## ## \n #   # \n #   # ")
+def x():
+    print(f"\n #   # \n #   # \n  # # \n   #  \n  # # \n #   # \n #   #")
+def z():
+    print(f" \n ##### \n     # \n    # \n   # \n  # \n # \n #####")
+def f():
+    print(f"\n##### \n# \n# \n### \n# \n# \n#")
+def v():
+    print(f"\n#   # \n#   # \n#   # \n#   # \n#   # \n # # \n  # ")
+
+i1 = (0)
+
+a1 = input()
+if a1 == "a":
+    a()
+if a1 == "b":
+    b()
+if a1 == "с":
+    c()
+if a1 == "s":
+    s()
+if a1 == "y":
+    y()
+if a1 == "k":
+    k()
+if a1 == "o":
+    o()
+if a1 == "l":
+    l()
+if a1 == "e":
+    e()
+if a1 == "d":
+    d()
+if a1 == "g":
+    g()
+if a1 == "h":
+    h()
+if a1 == "i":
+    i()
+if a1 == "j":
+    j()
+if a1 == "m":
+    m()
+if a1 == "p":
+    p()
+if a1 == "q":
+    q()
+if a1 == "t":
+    t()
+if a1 == "u":
+    u()
+if a1 == "w":
+    w()
+if a1 == "x":
+    x()
+if a1 == "z":
+    z()
+if a1 == "f":
+    f()
+if a1 == "v":
+    v()
+if a1 == "n":
+    n()
+if a1 == "r":
+    r()
+
+a2 = input()
+if a2 == "a":
+    a()
+if a2 == "b":
+    b()
+if a2 == "с":
+    c()
+if a2 == "s":
+    s()
+if a2 == "y":
+    y()
+if a2 == "k":
+    k()
+if a2 == "o":
+    o()
+if a2 == "l":
+    l()
+if a2 == "e":
+    e()
+if a2 == "d":
+    d()
+if a2 == "g":
+    g()
+if a2 == "h":
+    h()
+if a2 == "i":
+    i()
+if a2 == "j":
+    j()
+if a2 == "m":
+    m()
+if a2 == "p":
+    p()
+if a2 == "q":
+    q()
+if a2 == "t":
+    t()
+if a2 == "u":
+    u()
+if a2 == "w":
+    w()
+if a2 == "x":
+    x()
+if a2 == "z":
+    z()
+if a2 == "f":
+    f()
+if a2 == "v":
+    v()
+if a2 == "n":
+    n()
+if a2 == "r":
+    r()
+
+a3 = input()
+if a3 == "a":
+    a()
+if a3 == "b":
+    b()
+if a3 == "с":
+    c()
+if a3 == "s":
+    s()
+if a3 == "y":
+    y()
+if a3 == "k":
+    k()
+if a3 == "o":
+    o()
+if a3 == "l":
+    l()
+if a3 == "e":
+    e()
+if a3 == "d":
+    d()
+if a3 == "g":
+    g()
+if a3 == "h":
+    h()
+if a3 == "i":
+    i()
+if a3 == "j":
+    j()
+if a3 == "m":
+    m()
+if a3 == "p":
+    p()
+if a3 == "q":
+    q()
+if a3 == "t":
+    t()
+if a3 == "u":
+    u()
+if a3 == "w":
+    w()
+if a3 == "x":
+    x()
+if a3 == "z":
+    z()
+if a3 == "f":
+    f()
+if a3 == "v":
+    v()
+if a3 == "n":
+    n()
+if a3 == "r":
+    r()
+
+a4 = input()
+if a4 == "a":
+    a()
+if a4 == "b":
+    b()
+if a4 == "с":
+    c()
+if a4 == "s":
+    s()
+if a4 == "y":
+    y()
+if a4 == "k":
+    k()
+if a4 == "o":
+    o()
+if a4 == "l":
+    l()
+if a4 == "e":
+    e()
+if a4 == "d":
+    d()
+if a4 == "g":
+    g()
+if a4 == "h":
+    h()
+if a4 == "i":
+    i()
+if a4 == "j":
+    j()
+if a4 == "m":
+    m()
+if a4 == "p":
+    p()
+if a4 == "q":
+    q()
+if a4 == "t":
+    t()
+if a4 == "u":
+    u()
+if a4 == "w":
+    w()
+if a4 == "x":
+    x()
+if a4 == "z":
+    z()
+if a4 == "f":
+    f()
+if a4 == "v":
+    v()
+if a4 == "n":
+    n()
+if a4 == "r":
+    r()
+
+a5 = input()
+if a5 == "a":
+    a()
+if a5 == "b":
+    b()
+if a5 == "с":
+    c()
+if a5 == "s":
+    s()
+if a5 == "y":
+    y()
+if a5 == "k":
+    k()
+if a5 == "o":
+    o()
+if a5 == "l":
+    l()
+if a5 == "e":
+    e()
+if a5 == "d":
+    d()
+if a5 == "g":
+    g()
+if a5 == "h":
+    h()
+if a5 == "i":
+    i()
+if a5 == "j":
+    j()
+if a5 == "m":
+    m()
+if a5 == "p":
+    p()
+if a5 == "q":
+    q()
+if a5 == "t":
+    t()
+if a5 == "u":
+    u()
+if a5 == "w":
+    w()
+if a5 == "x":
+    x()
+if a5 == "z":
+    z()
+if a5 == "f":
+    f()
+if a5 == "v":
+    v()
+if a5 == "n":
+    n()
+if a5 == "r":
+    r()
+
+a6 = input()
+if a6 == "a":
+    a()
+if a6 == "b":
+    b()
+if a6 == "с":
+    c()
+if a6 == "s":
+    s()
+if a6 == "y":
+    y()
+if a6 == "k":
+    k()
+if a6 == "o":
+    o()
+if a6 == "l":
+    l()
+if a6 == "e":
+    e()
+if a6 == "d":
+    d()
+if a6 == "g":
+    g()
+if a6 == "h":
+    h()
+if a6 == "i":
+    i()
+if a6 == "j":
+    j()
+if a6 == "m":
+    m()
+if a6 == "p":
+    p()
+if a6 == "q":
+    q()
+if a6 == "t":
+    t()
+if a6 == "u":
+    u()
+if a6 == "w":
+    w()
+if a6 == "x":
+    x()
+if a6 == "z":
+    z()
+if a6 == "f":
+    f()
+if a6 == "v":
+    v()
+if a6 == "n":
+    n()
+if a6 == "r":
+    r()
+
+a7 = input()
+if a7 == "a":
+    a()
+if a7 == "b":
+    b()
+if a7 == "с":
+    c()
+if a7 == "s":
+    s()
+if a7 == "y":
+    y()
+if a7 == "k":
+    k()
+if a7 == "o":
+    o()
+if a7 == "l":
+    l()
+if a7 == "e":
+    e()
+if a7 == "d":
+    d()
+if a7 == "g":
+    g()
+if a7 == "h":
+    h()
+if a7 == "i":
+    i()
+if a7 == "j":
+    j()
+if a7 == "m":
+    m()
+if a7 == "p":
+    p()
+if a7 == "q":
+    q()
+if a7 == "t":
+    t()
+if a7 == "u":
+    u()
+if a7 == "w":
+    w()
+if a7 == "x":
+    x()
+if a7 == "z":
+    z()
+if a7 == "f":
+    f()
+if a7 == "v":
+    v()
+if a7 == "n":
+    n()
+if a7 == "r":
+    r()
+
+a8 = input()
+if a8 == "a":
+    a()
+if a8 == "b":
+    b()
+if a8 == "с":
+    c()
+if a8 == "s":
+    s()
+if a8 == "y":
+    y()
+if a8 == "k":
+    k()
+if a8 == "o":
+    o()
+if a8 == "l":
+    l()
+if a8 == "e":
+    e()
+if a8 == "d":
+    d()
+if a8 == "g":
+    g()
+if a8 == "h":
+    h()
+if a8 == "i":
+    i()
+if a8 == "j":
+    j()
+if a8 == "m":
+    m()
+if a8 == "p":
+    p()
+if a8 == "q":
+    q()
+if a8 == "t":
+    t()
+if a8 == "u":
+    u()
+if a8 == "w":
+    w()
+if a8 == "x":
+    x()
+if a8 == "z":
+    z()
+if a8 == "f":
+    f()
+if a8 == "v":
+    v()
+if a8 == "n":
+    n()
+if a8 == "r":
+    r()
